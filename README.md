b9cd308b6d167e2458d0ca73d0d6c32e7e82e483
Armando Ochoa <armaochoa99@gmail.com>
2025-03-17 15:57:22 -0600

    ajustes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# proyecto-spint-7
-Proyecto Sprint 7
+# Proyecto Sprint 7
+
+## Description
+Proyecto Sprint 7 es una aplicación que muestra graficos sobre la ventas de vehiculos.
+
+## Live Demo
+Puedes ver la aplicación en vivo en (https://proyecto-spint-7.onrender.com/).
--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import plotly.express as px
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
 disp_button = st.button('Construir dispersión') # crear un botón
+
+build_histogram = st.checkbox('Construir un histograma')
+build_disp = st.checkbox('Construir una dispersión')
         
 if hist_button: # al hacer clic en el botón
             # escribir un mensaje
@@ -17,6 +20,17 @@ if hist_button: # al hacer clic en el botón
             st.plotly_chart(fig, use_container_width=True)
 
 if disp_button:
+        st.write('Creación de grafico de dispersión para el conjunto de datos de anuncios de venta de coches')
+        fig = px.scatter(car_data, x="odometer", y="price")
+        st.plotly_chart(fig, use_container_width=True)
+
+
+if build_histogram:
+        st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+        fig = px.histogram(car_data, x="odometer")
+        st.plotly_chart(fig, use_container_width=True)
+
+if build_disp:
         st.write('Creación de grafico de dispersión para el conjunto de datos de anuncios de venta de coches')
         fig = px.scatter(car_data, x="odometer", y="price")
         st.plotly_chart(fig, use_container_width=True)
